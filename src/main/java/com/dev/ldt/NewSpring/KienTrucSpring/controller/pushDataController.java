a21fc101d8eb30e41305f2a793c05ac2454d1ab0
Le Danh Tien <danhtien2k4@gmail.com>
2025-03-21 15:47:20 +0700

    tạo thêm tính năng add

--- src/main/java/com/dev/ldt/NewSpring/KienTrucSpring/controller/pushDataController.java
+++ src/main/java/com/dev/ldt/NewSpring/KienTrucSpring/controller/pushDataController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,5 +25,17 @@ public class pushDataController {
             model.addAttribute("employeeList", employees);
             return "Hello";
             
+        }
+
+        @GetMapping("/add")
+        public String showAddEmployeeForm(Model model) {
+            model.addAttribute("employee", new Employee());
+            return "addEmployee";
+        }
+
+        @PostMapping("/add")
+        public String addEmployee(Employee employee) {
+            employeeService.saveEmployee(employee);
+            return "redirect:/employees";
         }
 }
